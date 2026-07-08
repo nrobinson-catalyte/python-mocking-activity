ac4fd38550fdf8cba98a406a063e5dc8511d0de8
nrobinson-catalyte <nrobinson@catalyte.io>
2026-07-08 11:55:34 -0500

    Requirements

--- test_greeter.py
+++ test_greeter.py
@@ -23,3 +23,21 @@ def test_greet_evening_unknown_user(mocker):
 # Add a test that checks greet() still works correctly at
 # exactly hour=5 (the boundary between "evening" and "morning").
 # What does the greeting say? Is that the right behaviour?
+
+#Requirements#
+#1. Complete Set Up follow 4 steps listed. -DONE
+#2. Open test_greeter.py and examine 3 test functions.
+#3. Create a mock test for test_greet_morning that follows three rules.
+    #Create a Greeter instance.
+    #Use mocker.patch.object to mock both helper methods.
+    #Call greet() and assert the result.
+#4. Create a mock test for test_greet_afternoon that follows three rules.
+     #Create a Greeter instance.
+    #Use mocker.patch.object to mock both helper methods.
+    #Call greet() and assert the result.   
+#4. Create a mock test for test_greet_evening_unknown_use that follows three rules.
+    #Create a Greeter instance.
+    #Use mocker.patch.object to mock both helper methods.
+    #Call greet() and assert the result.
+#5 Confirm that all three test pass & -v glag prints each test name.
+#6 Create a fourth Add a fourth test that checks the boundary at hour 5 — the exact point where "evening" ends and "morning" begins.
